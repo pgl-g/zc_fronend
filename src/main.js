80cd9b529bf6bafa5b3e325982c9e15f17937dc5
guxiaobiao <guxiaobiao@xinxuan.net>
2024-09-23 15:08:40 +0800

    feat: 配置侧边栏导航

--- src/main.js
+++ src/main.js
@@ -1,10 +1,17 @@
-import './assets/main.css'
+// import './assets/main.css'
 import ElementPlus from 'element-plus'
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 import App from './App.vue'
 import router from './router'
+
+// 导入element plus国际化
+// import zhCn from 'element-plus/dist/locale/zh-cn.mjs'
+// svg图标
+// import 'virtual:svg-icons-register'
+// 引入全局样式
+import '@/assets/css/common.scss'
 
 import 'element-plus/dist/index.css'
 
@@ -18,4 +25,9 @@ app.use(ElementPlus)
 for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
   app.component(key, component)
 }
+
+// app.use(ElementPlus, {
+//   locale: zhCn,
+// })
+
 app.mount('#app')
--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
-import LoginView from '../views/login/index.vue'
+import LoginView from '@/views/login/index.vue';
+import menu from '@/views/menu/inedx.vue';
 
 const router = createRouter({
   history: createWebHashHistory(import.meta.env.BASE_URL),
@@ -13,6 +14,10 @@ const router = createRouter({
       path: '/',
       name: '/login',
       component: LoginView
+    }, {
+      name: 'menu',
+      path: '/menu',
+      component: menu
     }
   ]
 })
